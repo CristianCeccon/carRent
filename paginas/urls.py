d786e83a24cd0a1f734385ff97ae21ef5a79081c
Cristian Ceccon <Cristian_ceccon45@hotmail.com>
2022-05-09 21:02:40 -0300

    Inicio cadastros

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,12 +1,12 @@
-from os import path
+from django.urls import path
 from .views import CidadeCreate, PessoaCreate
 from .views import CidadeUpdate, PessoaUpdate
 
 urlpatterns = [
-    path('paginas/cidade/', CidadeCreate.as_view(), name='cadastrar-cidade'),
-    path('paginas/pessoa/', PessoaCreate.as_view(), name='cadastrar-pessoa'),
+    path('cadastrar/cidade/', CidadeCreate.as_view(), name='cadastrar-cidade'),
+    path('cadastrar/pessoa/', PessoaCreate.as_view(), name='cadastrar-pessoa'),
 
-    path('editar/estado/<int:pk>/', PessoaUpdate.as_view(), name='editar-pessoa'),
+    path('editar/pessoa/<int:pk>/', PessoaUpdate.as_view(), name='editar-pessoa'),
     path('editar/cidade/<int:pk>/', CidadeUpdate.as_view(), name='editar-cidade'),
 
 ]
